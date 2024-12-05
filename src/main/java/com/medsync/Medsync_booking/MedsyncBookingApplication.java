141dcdd4f875b070fc950a5995dacd5d8afa7f25
Induwara-rathnayaka <rathnay-ps20245@stu.kln.ac.lk>
2024-12-05 15:17:45 +0530

    first  commit

---
+++ src/main/java/com/medsync/Medsync_booking/MedsyncBookingApplication.java
@@ -0,0 +1,13 @@
+package com.medsync.Medsync_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsyncBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsyncBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medsync/Medsync_booking/MedsyncBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medsync.Medsync_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsyncBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
